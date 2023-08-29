2999e0659f4d18ffd5bb223fd30514291cebcdd9
chris00Code <student.christian.lurger@gmail.com>
2023-08-29 10:48:17 +0200

    starting organising

--- exchanger/exchanger_creator.py
+++ exchanger/exchanger_creator.py
@@ -13,7 +13,7 @@ def auto_create_exchanger(flow_1: Flow = None, flow_2: Flow = None, assembly: As
         layout_cols = baffles.number_baffles + 1
     layout_rows = assembly.tube_passes
 
-    if layout_cols ==2 and layout_rows ==1:
+    if layout_cols == 2 and layout_rows == 1:
         raise ValueError("number of baffle and tube passes are available")
 
     layout_shape = layout_rows, layout_cols
