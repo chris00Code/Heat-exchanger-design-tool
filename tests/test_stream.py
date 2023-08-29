2b5ae3c39a6a54618feae2c3c52b3956482c9958
chris00Code <student.christian.lurger@gmail.com>
2023-08-29 19:26:21 +0200

    stream test cleanup

--- tests/test_stream.py
+++ tests/test_stream.py
@@ -3,7 +3,7 @@ import pyfluids as pyf
 from exchanger.stream import Fluid, Flow
 
 
-class FluidTests(unittest.TestCase):
+class TestFluid(unittest.TestCase):
 
     def test_fluid_init(self):
         with self.assertRaises(TypeError, msg='Fluid instance was created with no parameter'):
@@ -59,60 +59,83 @@ class FluidTests(unittest.TestCase):
         fluid.temperature = value
         self.assertEqual(fluid.temperature, value, msg='negative degree temperature setting not working')
 
-    def test_fluid_print(self):
-        fluid = Fluid("Water")
-        fluid.temperature = 350
-        fluid.pressure = 10
-        print(fluid)
-
     def test_fluid_clone(self):
         fluid = Fluid("Water")
         new_fluid = fluid.clone()
-        self.assertNotEqual(fluid, new_fluid)
+        self.assertNotEqual(fluid, new_fluid, msg='fluid cloning not working, fluid has same hash value')
         new_fluid.pressure = 100e3
 
         fluid = Fluid("Water", temperature=280)
         new_fluid = fluid.clone()
-        self.assertNotEqual(fluid, new_fluid)
+        self.assertNotEqual(fluid, new_fluid, msg='fluid cloning not working, fluid has same hash value')
         new_fluid.pressure = 100e3
         new_fluid.temperature = 350
         fluid.pressure = 150e3
 
-        self.assertEqual(fluid.temperature, 280)
-        self.assertEqual(new_fluid.temperature, 350)
-        self.assertEqual(fluid.pressure, 150e3)
-        self.assertEqual(new_fluid.pressure, 100e3)
+        self.assertEqual(fluid.temperature, 280, msg='fluid cloning not working, temperature is still same object')
+        self.assertEqual(new_fluid.temperature, 350, msg='fluid cloning not working, temperature is still same object')
+        self.assertEqual(fluid.pressure, 150e3, msg='fluid cloning not working, pressure is still same object')
+        self.assertEqual(new_fluid.pressure, 100e3, msg='fluid cloning not working, pressure is still same object')
+
+    def test_fluid_str(self):
+        expected_output = r'^Fluid: title = \w+, id = \d+\n\tp = \d+(\.\d+)? Pa\n\tt = -?\d+(\.\d+)? °C'
+
+        fluid = Fluid("Air", 210.20, 60)
+        self.assertRegex(str(fluid), expected_output, msg='fluid str has not expected pattern')
+        fluid = Fluid("Water")
+        self.assertRegex(str(fluid), expected_output, msg='fluid str has not expected pattern')
+
+
+class TestFlow(unittest.TestCase):
+    def setUp(self):
+        self.fluid_water = Fluid("Water")
 
     def test_flow_init(self):
-        with self.assertRaises(TypeError):
+        with self.assertRaises(TypeError, msg='Flow instance was created with no parameter'):
             flow = Fluid()
-        fluid = Fluid("Water")
-        flow = Flow(fluid)
-        self.assertEqual(flow.in_fluid, fluid)
 
+        flow = Flow(self.fluid_water)
+        self.assertEqual(flow.in_fluid, self.fluid_water,
+                         msg='in fluid object is not the same object as in the constructor assigned')
+
+    def test_flow_rate(self):
+        flow = Flow(self.fluid_water)
         flow.volume_flow = 10
-        self.assertEqual(flow.volume_flow, 10)
+        self.assertEqual(flow.volume_flow, 10, msg='volume flow setter is not working')
 
-        flow.mass_flow = 15
-        self.assertEqual(flow.mass_flow, 15)
-        in_density = fluid.density
-        self.assertEqual(flow.volume_flow, 15 / in_density)
+        value = 15
+        flow.mass_flow = value
+        self.assertEqual(flow.mass_flow, value, msg='mass flow setter is not working')
+        in_density = self.fluid_water.density
+        self.assertEqual(flow.volume_flow, value / in_density,
+                         msg='volume flow calculating by mass flow and density not correct')
 
     def test_flow_phasechange(self):
-        fluid = Fluid("Water")
-        flow = Flow(fluid, 15)
-        flow.pressure_loss = -5000
-        with self.assertRaises(Warning):
+        flow = Flow(self.fluid_water, 15)
+        value = -5000
+        flow.pressure_loss = value
+        self.assertEqual(flow.in_fluid.pressure, 101325, msg='in fluid pressure not correct')
+        self.assertEqual(flow.out_fluid.pressure, 101325 - value,
+                         msg='out fluid pressure not correct calculated/set by pressure loss')
+        with self.assertWarnsRegex(Warning, 'the phase changes, this could lead to some problems',
+                                   msg='no warning when the phase change is set'):
             flow.out_temperature = 250 + 273.15
+        with self.assertWarnsRegex(Warning, 'the phase changes, this could lead to some problems',
+                                   msg='no warning when the phase change is used'):
+            q = flow.heat_flow
+
         flow.out_temperature = 101 + 273.15
+        q = flow.heat_flow
 
     def test_flow_heatflow(self):
         fluid = Fluid("Water", temperature=273.15 + 15)
         flow = Flow(fluid, 0.33)
-        self.assertAlmostEqual(flow.heat_capacity_flow, 1382, 0)
-        self.assertEqual(flow.heat_flow, 0)
+        self.assertAlmostEqual(flow.heat_capacity_flow, 1382, 0, msg='heat capacity flow not correct')
+        self.assertEqual(flow.in_fluid.temperature - flow.out_temperature, 0, msg='temperature delta is not 0')
+        self.assertEqual(flow.heat_flow, 0, msg='heat flow is not 0, if temperature delta is 0')
         flow.out_temperature = 273.15 + 23.11
-        self.assertAlmostEqual(flow.heat_flow, -11.2e3, delta=0.1e3)
+        self.assertNotEqual(flow.in_fluid.temperature - flow.out_temperature, 0, msg='temperature delta is 0')
+        self.assertAlmostEqual(flow.heat_flow, -11.2e3, delta=0.1e3, msg='heat flow was not calculated correct')
 
     def test_flow_clone(self):
         fluid = Fluid("Water")
