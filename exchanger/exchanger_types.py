f301f834c803f8fd8b540211056eabaf4ab1aa16
chris00Code <student.christian.lurger@gmail.com>
2024-01-07 12:46:05 +0100

    exchanger_types.py docstring

--- exchanger/exchanger_types.py
+++ exchanger/exchanger_types.py
@@ -16,6 +16,27 @@ from .parts import Assembly
 
 
 class ExchangerTwoFlow(ExchangerNetwork):
+    """
+        A class representing a two-flow heat exchanger network.
+
+        Attributes:
+            flow_orders (list): A list of valid flow orders for the two flows.
+            auto_adjust (bool): A flag indicating whether to automatically adjust temperatures and fluid properties.
+            layout_matrix (np.ndarray): The layout matrix representing the arrangement of heat exchangers.
+            in_flow_1 (Flow): The input flow for the first flow path.
+            in_flow_2 (Flow): The input flow for the second flow path.
+            flow_order_1 (str): The flow order for the first flow path.
+            flow_order_2 (str): The flow order for the second flow path.
+
+
+        Args:
+            layout_matrix (np.ndarray): The layout matrix representing the arrangement of heat exchangers.
+            flow_1 (Flow): The input flow for the first flow path.
+            flow_order_1 (str): The flow order for the first flow path.
+            flow_2 (Flow): The input flow for the second flow path.
+            flow_order_2 (str): The flow order for the second flow path.
+
+       """
     flow_orders = ['ul2r', 'dl2r', 'ur2l', 'dr2l', 'ul2d', 'ur2d', 'dl2u', 'dr2u']
     auto_adjust = True
 
@@ -33,6 +54,17 @@ class ExchangerTwoFlow(ExchangerNetwork):
 
     @property
     def layout_matrix(self):
+        """
+        Get or set the layout matrix representing the arrangement of heat exchangers.
+
+        Setter enforces the matrix to be a numpy array or None.
+
+        Args:
+            value (np.ndarray or None): A matrix representing the layout of the heat exchanger.
+
+        Raises:
+            NotImplementedError: If the input is not a numpy array or None.
+        """
         return self._layout_matrix
 
     @layout_matrix.setter
@@ -45,6 +77,15 @@ class ExchangerTwoFlow(ExchangerNetwork):
 
     @property
     def exchangers(self):
+        """
+        Get or set the list of exchangers in the network.
+
+        Args:
+            value (list or HeatExchanger): A list of heat exchangers or a single heat exchanger.
+
+        Raises:
+            NotImplementedError: If the provided value is not a list or a HeatExchanger object.
+        """
         return self._exchangers_flattened[0]
 
     @exchangers.setter
@@ -56,6 +97,15 @@ class ExchangerTwoFlow(ExchangerNetwork):
 
     @property
     def in_flow_1(self):
+        """
+        Get or set the input flow for the first flow path.
+
+        Args:
+            value (Flow): The new value for the input flow.
+
+        Raises:
+            NotImplementedError: If the provided value is not a Flow object.
+        """
         return self.input_flows[0]
 
     @in_flow_1.setter
@@ -66,6 +116,15 @@ class ExchangerTwoFlow(ExchangerNetwork):
 
     @property
     def in_flow_2(self):
+        """
+        Get or set the input flow for the second flow path.
+
+        Args:
+            value (Flow): The new value for the input flow.
+
+        Raises:
+            NotImplementedError: If the provided value is not a Flow object.
+        """
         return self.input_flows[1]
 
     @in_flow_2.setter
@@ -76,6 +135,15 @@ class ExchangerTwoFlow(ExchangerNetwork):
 
     @property
     def out_flow_1(self):
+        """
+        Get or set the output flow for the first flow path.
+
+        Parameters:
+            - value (Flow): The new value for the output flow.
+
+        Raises:
+            - NotImplementedError: If the provided value is not a Flow object.
+        """
         self._flatten()
         return self.output_flows[0]
 
@@ -86,6 +154,15 @@ class ExchangerTwoFlow(ExchangerNetwork):
 
     @property
     def out_flow_2(self):
+        """
+        Get or set the output flow for the second flow path.
+
+        Parameters:
+            - value (Flow): The new value for the output flow.
+
+        Raises:
+            - NotImplementedError: If the provided value is not a Flow object.
+        """
         return self.output_flows[1]
 
     @out_flow_2.setter
@@ -97,6 +174,15 @@ class ExchangerTwoFlow(ExchangerNetwork):
 
     @property
     def flow_order_1(self):
+        """
+        Get or set the flow order for the first flow path.
+
+        Args:
+            value (str): The new value for the flow order.
+
+        Raises:
+            NotImplementedError: If the provided value is not in the list of flow orders or None.
+        """
         return self._flow_order_1
 
     @flow_order_1.setter
@@ -116,6 +202,15 @@ class ExchangerTwoFlow(ExchangerNetwork):
 
     @property
     def flow_order_2(self):
+        """
+        Get or set the flow order for the first flow path.
+
+        Args:
+            value (str): The new value for the flow order.
+
+        Raises:
+            NotImplementedError: If the provided value is not in the list of flow orders or None.
+        """
         return self._flow_order_2
 
     @flow_order_2.setter
@@ -134,11 +229,24 @@ class ExchangerTwoFlow(ExchangerNetwork):
             raise NotImplementedError
 
     def flow_orders_str(self):
+        """
+        Get a string representation of the flow orders.
+
+        Returns:
+            str: A string representation of the flow orders.
+        """
         output = f"flow order: flow_1:{self.flow_order_1}\tflow_2:{self.flow_order_2}\n"
         return output
 
     @property
     def heat_flows(self):
+        """
+        Get the total heat flows in the network.
+
+        Returns:
+            Tuple: A tuple containing the total heat flows for flow path 1 and flow path 2.
+
+        """
         q_1, q_2 = 0, 0
         for ex in self.exchangers:
             q_1 += ex.heat_flows[0]
@@ -147,6 +255,13 @@ class ExchangerTwoFlow(ExchangerNetwork):
 
     @property
     def cell_numbers(self):
+        """
+        Get the number of cells in the layout matrix.
+
+        Returns:
+            int: The number of cells in the layout matrix.
+
+        """
         layout_matrix = self.layout_matrix
         if layout_matrix is None:
             value = 0
@@ -156,6 +271,13 @@ class ExchangerTwoFlow(ExchangerNetwork):
 
     @property
     def total_transferability(self):
+        """
+        Get the total heat transferability of the exchangers.
+
+        Returns:
+            float: The total heat transferability of the exchangers.
+
+        """
         values = []
         for ex in self.exchangers:
             values.append(ex.heat_transferability)
@@ -167,11 +289,25 @@ class ExchangerTwoFlow(ExchangerNetwork):
 
     @property
     def nodes(self):
+        """
+        Get a list of nodes in the network.
+
+        Returns:
+            list: A list containing input flows, exchangers, and output flows.
+
+        """
         value = self.input_flows + self.exchangers + self.output_flows
         return value
 
     @property
     def paths(self):
+        """
+        Get the paths of nodes in the network.
+
+        Returns:
+            Tuple: A tuple containing the paths for flow path 1 and flow path 2.
+
+        """
         try:
             value = self._paths
         except AttributeError:
@@ -180,9 +316,23 @@ class ExchangerTwoFlow(ExchangerNetwork):
         return value
 
     def _fill(self):
+        """
+        Fill the layout matrix with HeatExchanger objects.
+
+        This private method is responsible for filling the layout matrix with HeatExchanger objects. It is called during
+        the initialization process.
+
+        """
         pass
 
     def _flatten(self):
+        """
+        Flatten the layout matrix and set in and out fluids for exchangers.
+
+        This private method flattens the layout matrix according to flow orders, sets in and out fluids for exchangers,
+        and sets the output flows.
+
+        """
         try:
             flattened_1 = flatten(self.layout_matrix, self.flow_order_1)
             flattened_2 = flatten(self.layout_matrix, self.flow_order_2)
@@ -205,8 +355,10 @@ class ExchangerTwoFlow(ExchangerNetwork):
 
     def _set_in_out_fluids_in_exchangers(self):
         """
-        setting in and out fluids according flow order
-        :return:
+        Set in and out fluids for exchangers based on flow order.
+
+        This private method sets the in and out fluids for exchangers according to the specified flow order.
+
         """
         for i, ex in enumerate(self._exchangers_flattened[0]):
             if i > 0:
@@ -218,6 +370,12 @@ class ExchangerTwoFlow(ExchangerNetwork):
             prev_out_fluid = ex.flow_2.out_fluid
 
     def _extract_node_paths(self):
+        """
+        Extract node paths for flow paths 1 and 2.
+
+        This private method extracts node paths for flow paths 1 and 2 and sets them to the _paths attribute.
+
+        """
         self._flatten()
 
         path_1 = self._exchangers_flattened[0].copy()
@@ -234,6 +392,13 @@ class ExchangerTwoFlow(ExchangerNetwork):
 
     @property
     def adjacency(self):
+        """
+        Get the adjacency matrices for flow paths 1 and 2.
+
+        Returns:
+            Tuple: A tuple containing the adjacency matrices for flow paths 1 and 2.
+
+        """
         try:
             value = self._adj_1, self._adj_2
         except AttributeError:
@@ -242,6 +407,13 @@ class ExchangerTwoFlow(ExchangerNetwork):
         return value
 
     def _matrix_representation(self):
+        """
+        Generate adjacency matrices for flow paths 1 and 2.
+
+        This private method utilizes the NetworkX library to create directed graphs for flow paths 1 and 2 and generate
+        adjacency matrices. The adjacency matrices are then set to the _adj_1 and _adj_2 attributes.
+
+        """
         path_1, path_2 = self.paths
         nodes = self.nodes
         graph_1 = nx.DiGraph()
@@ -259,6 +431,13 @@ class ExchangerTwoFlow(ExchangerNetwork):
 
     @property
     def structure_matrix(self):
+        """
+        Get the structure matrix of the exchanger network.
+
+        Returns:
+            numpy.ndarray: Structure matrix of the exchanger network.
+
+        """
         s11 = self.adjacency[0][2:-2, 2:-2]
         s22 = self.adjacency[1][2:-2, 2:-2]
         zeros = np.zeros_like(s11)
@@ -267,6 +446,13 @@ class ExchangerTwoFlow(ExchangerNetwork):
 
     @property
     def input_matrix(self):
+        """
+        Get the input matrix of the exchanger network.
+
+        Returns:
+            numpy.ndarray: Input matrix of the exchanger network.
+
+        """
         in_1 = self.adjacency[0][:2, 2:-2]
         in_2 = self.adjacency[1][:2, 2:-2]
         input = np.hstack((in_1, in_2)).T
@@ -274,6 +460,13 @@ class ExchangerTwoFlow(ExchangerNetwork):
 
     @property
     def output_matrix(self):
+        """
+        Get the output matrix of the exchanger network.
+
+        Returns:
+            numpy.ndarray: Output matrix of the exchanger network.
+
+        """
         out_1 = self.adjacency[0][2:-2, -2:]
         out_2 = self.adjacency[1][2:-2, -2:]
         output = np.vstack((out_1, out_2)).T
@@ -281,10 +474,27 @@ class ExchangerTwoFlow(ExchangerNetwork):
 
     @property
     def phi_matrix(self):
+        """
+        Get the phi matrix of the exchanger network.
+
+        Returns:
+            numpy.ndarray: Phi matrix of the exchanger network.
+
+        """
         _ = self.structure_matrix
         return super().phi_matrix
 
     def _adjust_temperatures(self, iterations=1):
+        """
+        Adjust temperatures in the exchanger network and update fluid parameters.
+
+        This method iteratively adjusts temperatures in the exchanger network, which leads to a more accurate calculation
+        of fluid parameters. The adjustment is performed based on the specified flow orders.
+
+        Args:
+            iterations (int, optional): Number of iterations for temperature adjustment. Defaults to 1.
+
+        """
         for i in range(iterations):
             temperature_outputs = self.temperature_outputs[1]
             try:
@@ -316,6 +526,13 @@ class ExchangerTwoFlow(ExchangerNetwork):
                 prev_out_temp = ex.flow_2.out_fluid.temperature
 
     def temperature_outputs_str(self):
+        """
+        Get a string representation of temperature outputs in °C.
+
+        Returns:
+            str: String representation of temperature outputs in °C.
+
+        """
         try:
             return f"\ttemperature outputs: flow 1=%.2f °C,\tflow 2=%.2f °C\n" % (
                 self.temperature_outputs[1][0, 0] - 273.15, self.temperature_outputs[1][1, 0] - 273.15)
@@ -323,6 +540,14 @@ class ExchangerTwoFlow(ExchangerNetwork):
             return ""
 
     def vis_heat_flow(self, ax=None, **ax_parameters):
+        """
+        Visualize the heat flow in the exchanger network.
+
+        Args:
+            ax (matplotlib.axes.Axes, optional): Matplotlib axes to plot on. If None, a new figure and axes will be created.
+            **ax_parameters: Additional keyword arguments for Matplotlib axes.
+
+        """
         par_matrix = heat_flow_repr(self.layout_matrix)
 
         vmin = ax_parameters.pop('vmin', 0)
@@ -348,9 +573,21 @@ class ExchangerTwoFlow(ExchangerNetwork):
 
 
     def vis_temperature_adjustment_development(self):
+        """
+        Visualize the development of temperature adjustment over iterations.
+
+        """
         super()._vis_temperature_adjusment_development(self._temperature_adjustment_development)
 
     def vis_flow_temperature_development(self, ax=None, **ax_parameters):
+        """
+        Visualize the development of flow temperatures in the exchanger network.
+
+        Args:
+            ax (matplotlib.axes.Axes, optional): Matplotlib axes to plot on. If None, a new figure and axes will be created.
+            **ax_parameters: Additional keyword arguments for Matplotlib axes.
+
+        """
         temp_1, temp_2 = [], []
         for ex in self._exchangers_flattened[0]:
             temp_1.append(ex.flow_1.in_fluid.temperature)
@@ -364,10 +601,26 @@ class ExchangerTwoFlow(ExchangerNetwork):
 
     @staticmethod
     def input_arrangements():
+        """
+        Get all possible input flow order arrangements.
+
+        Returns:
+            list: List of tuples representing all possible input flow order arrangements.
+
+        """
         result = list(permutations(ExchangerTwoFlow.flow_orders, 2))
         return result
 
     def __repr__(self):
+        """
+        String representation of the ExchangerTwoFlow object.
+
+        If auto_adjust is True, the temperature adjustment is automatically performed before generating the string representation.
+
+        Returns:
+            str: String representation of the object.
+
+        """
         if self.auto_adjust:
             try:
                 self._adjust_temperatures(5)
@@ -377,6 +630,18 @@ class ExchangerTwoFlow(ExchangerNetwork):
 
 
 class ExchangerEqualCells(ExchangerTwoFlow):
+    """
+    A class representing a two-flow heat exchanger network with cells of equal properties.
+
+    Attributes:
+        exchangers_type (str): The type of heat exchangers used in the network.
+        assembly (Assembly): The assembly object containing all constructive parameters for the network.
+        total_transferability (float): The total heat transferability of the exchangers.
+
+    Methods:
+    - input_arrangements(): Returns a list of possible input flow arrangements.
+
+    """
     def __init__(self, shape: tuple = (0, 0),
                  exchangers_type: str = 'HeatExchanger',
                  flow_1: Flow = None, flow_order_1: str = None,
@@ -392,6 +657,16 @@ class ExchangerEqualCells(ExchangerTwoFlow):
 
     @property
     def exchangers_type(self):
+        """
+        Get or set the type of heat exchangers used in the network.
+
+        Args:
+            value (str): The type of heat exchangers.
+
+        Raises:
+            NotImplementedError: If the provided value is not in the list of available class names.
+
+        """
         return self._exchangers_type
 
     @exchangers_type.setter
@@ -403,6 +678,16 @@ class ExchangerEqualCells(ExchangerTwoFlow):
 
     @property
     def shape(self):
+        """
+        Get or set the shape of the layout matrix.
+
+        Args:
+            value (tuple): The shape of the layout matrix.
+
+        Raises:
+            NotImplementedError: If the provided value is not a tuple.
+
+        """
         return self.layout_matrix.shape
 
     @shape.setter
@@ -411,6 +696,16 @@ class ExchangerEqualCells(ExchangerTwoFlow):
 
     @property
     def layout_matrix(self):
+        """
+        Get or set the layout matrix of the network.
+
+        Args:
+            shape (tuple): The shape of the layout matrix.
+
+        Raises:
+            NotImplementedError: If the provided value is not a tuple.
+
+        """
         return super().layout_matrix
 
     @layout_matrix.setter
@@ -422,6 +717,16 @@ class ExchangerEqualCells(ExchangerTwoFlow):
 
     @property
     def assembly(self):
+        """
+        Get or set the assembly object containing all constructive parameters for the network.
+
+        Args:
+            value (Assembly or NotImplemented): The assembly object.
+
+        Raises:
+            NotImplementedError: If the provided value is not an Assembly object or NotImplemented.
+
+        """
         return self._assembly
 
     @assembly.setter
@@ -433,6 +738,16 @@ class ExchangerEqualCells(ExchangerTwoFlow):
 
     @property
     def total_transferability(self):
+        """
+        Get or set the total heat transferability of the exchangers.
+
+        Args:
+            value (float): The total heat transferability.
+
+        Raises:
+            NotImplementedError: If the provided value is not a float.
+
+        """
         def_value = self._total_transferability
         if self.assembly is NotImplemented:
             calc_value = NotImplemented
@@ -447,8 +762,8 @@ class ExchangerEqualCells(ExchangerTwoFlow):
 
     def _fill(self):
         """
-        fills the Layout with Heat Exchanger objects
-        """
+         Fill the layout matrix with Heat Exchanger objects of equal type.
+         """
         try:
             ex_class = globals()[self.exchangers_type]
 
@@ -462,6 +777,19 @@ class ExchangerEqualCells(ExchangerTwoFlow):
             pass
 
     def __new_ex(self, ex_class):
+        """
+        Create a new instance of a heat exchanger of the specified class.
+
+        Args:
+            ex_class (class): The class of the heat exchanger.
+
+        Returns:
+            HeatExchanger: The new instance of the heat exchanger.
+
+        Raises:
+            ValueError: If input flows are not implemented yet.
+
+        """
         if not self.input_flows == [NotImplemented, NotImplemented]:
             flow_1, flow_2 = self.input_flows[0].clone(), self.input_flows[1].clone()
             ex = ex_class(flow_1, flow_2)
@@ -471,6 +799,13 @@ class ExchangerEqualCells(ExchangerTwoFlow):
             raise ValueError("Flows not implemented yet")
 
     def __set_transferability(self, exchanger):
+        """
+        Set the heat transferability for the given exchanger.
+
+        Args:
+            exchanger (HeatExchanger): The heat exchanger to set the transferability for.
+
+        """
         try:
             exchanger.heat_transferability = self.total_transferability / self.cell_numbers
         except TypeError:
